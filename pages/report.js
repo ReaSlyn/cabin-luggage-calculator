26db31b0576cba91ff8f1c1b61dee80d9887aa01
ReaSlyn <thanh-long.pham@hetic.net>
2022-05-21 11:01:01 +0200

    made project structure

---
+++ pages/report.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "../styles/Report.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Cabin luggage calculator - Resume</title>
+        <meta name="description" content="Cabin luggage calculator - Resume" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
